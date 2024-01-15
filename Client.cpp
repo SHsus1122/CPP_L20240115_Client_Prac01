c17d67e3162a2e1da52f7f3c664c55f24520375d
SHsus1122 <116439064+SHsus1122@users.noreply.github.com>
2024-01-15 10:34:59 +0900

    주석 추가

--- Client.cpp
+++ Client.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int main()
 {
+	// 1. Socket 시작 작업
 	WSAData wsaData;
 	int Result = WSAStartup(MAKEWORD(2, 2), &wsaData);
 
@@ -20,15 +21,21 @@ int main()
 		exit(-1);
 	}
 
+	// 2. Server 소켓 주소값 작성 및 초기화
+	// SOCKADDR_IN 로 사용한 즉, (대문자) 방식은 윈도우만 지원합니다.
+	// struct sockaddr_in 이렇게 작성한 방식은 C 언어에서 지원하기에 이 방식이 더 좋습니다.
 	struct sockaddr_in ServerSockAddr;
 	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
 
+	// 3. Server 소켓 주소 설정
 	ServerSockAddr.sin_family = AF_INET;
 	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	ServerSockAddr.sin_port = htons(5001);
 
+	// 4. Server 소켓 생성 및 설정
 	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
+	// 5. Server 소켓 연결 작업
 	Result = connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
 	if (Result == SOCKET_ERROR)
 	{
@@ -36,19 +43,21 @@ int main()
 		exit(-1);
 	}
 
-
+	// 6. Server 로 보낼 내용(Buffer) 작성
 	char Buffer[1024] = { 0, };
 	sprintf(Buffer, "give me message.");
 
 	int SentByte = send(ServerSocket, Buffer, (int)(strlen(Buffer) + 1), 0);
+
 	cout << "Send Data" << endl;
 	
-
+	// 7. Server 로부터 받을 내용(Buffer) 작성
 	char RecvBuffer[1024] = { 0, };
 	int RecvByte = recv(ServerSocket, RecvBuffer, sizeof(RecvBuffer), 0);
 
 	cout << "Server Sent : " << RecvBuffer << endl;
 
+	// 8. 소켓 종료 및 정리
 	closesocket(ServerSocket);
 
 	WSACleanup();
