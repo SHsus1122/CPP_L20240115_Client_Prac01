a67f7104b12619076b253745ead2bef7dbba9099
SHsus1122 <116439064+SHsus1122@users.noreply.github.com>
2024-01-15 10:03:57 +0900

    TCPServer Client 보내고 정해진 답 받기 예제 코드 완료

--- Client.cpp
+++ Client.cpp
@@ -1,9 +1,57 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <iostream>
+#include <WinSock2.h>
+#include <string>
+
+#pragma comment(lib, "ws2_32")
 
 using namespace std;
 
 int main()
 {
+	WSAData wsaData;
+	int Result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	if (Result != 0)
+	{
+		cout << "Winsock dll error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	struct sockaddr_in ServerSockAddr;
+	memset(&ServerSockAddr, 0, sizeof(ServerSockAddr));
+
+	ServerSockAddr.sin_family = AF_INET;
+	ServerSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	ServerSockAddr.sin_port = htons(5001);
+
+	SOCKET ServerSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	Result = connect(ServerSocket, (SOCKADDR*)&ServerSockAddr, sizeof(ServerSockAddr));
+	if (Result == SOCKET_ERROR)
+	{
+		cout << "can't connect. : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+
+	char Buffer[1024] = { 0, };
+	sprintf(Buffer, "give me message.");
+
+	int SentByte = send(ServerSocket, Buffer, (int)(strlen(Buffer) + 1), 0);
+	cout << "Send Data" << endl;
+	
+
+	char RecvBuffer[1024] = { 0, };
+	int RecvByte = recv(ServerSocket, RecvBuffer, sizeof(RecvBuffer), 0);
+
+	cout << "Server Sent : " << RecvBuffer << endl;
+
+	closesocket(ServerSocket);
+
+	WSACleanup();
 
 	return 0;
 }
